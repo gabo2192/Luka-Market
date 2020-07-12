33e842b14e09bdbf4c780dbb4d06b764ef21d100
Gabriel Rojas <35940919+gabo2192@users.noreply.github.com>
2020-07-12 15:49:21 -0500

    feat: remove .cache

--- site/.cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---packages-gatsby-theme-lukamarket-src-templates-market-js": () => import("./../../../packages/gatsby-theme-lukamarket/src/templates/market.js" /* webpackChunkName: "component---packages-gatsby-theme-lukamarket-src-templates-market-js" */),
-  "component---packages-gatsby-theme-lukamarket-src-templates-markets-js": () => import("./../../../packages/gatsby-theme-lukamarket/src/templates/markets.js" /* webpackChunkName: "component---packages-gatsby-theme-lukamarket-src-templates-markets-js" */)
-}
-
